2fe26f51a92f3a5c276fc5aeca59a71da4d1be1a
yuanwenbin <759992680@qq.com>
2018-11-09 09:53:39 +0800

    Initial commit

---
+++ src/main/java/com/hoperun/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.hoperun.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("app/")
+public class UserController {
+    @RequestMapping("test")
+    @ResponseBody
+    public String testMVC(Model modell){
+        return  "success";
+    }
+
+//    public staitc
+//    public staitc
+}
